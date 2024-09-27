3f06f132527f3fd678b7f230feabf9576647179b
Harsh <Harshnagarwal@yahoo.in>
2024-09-27 17:56:14 +0530

    Courseinfo completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,19 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   
   return (
     <div>
-      <Header course={course}/>
-      <Content items={[part1, exercises1]}/>
-      <Content items={[part2, exercises2]}/>
-      <Content items={[part3, exercises3]}/>
-      <Total arr={[exercises1, exercises2, exercises3]} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -29,7 +37,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.items[0]} exercises={props.items[1]} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
     )
 }
@@ -43,9 +53,11 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  let sum = 0
+  props.parts.forEach(value => sum = sum + value.exercises) 
   return (
     <>
-      <p>Number of exercises {props.arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}</p>
+      <p>Number of exercises {sum}</p>
     </>
     )
 }
